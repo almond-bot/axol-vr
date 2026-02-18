1747d85644457122d25fd0f733be7d672a3bcb54
Shawn Patel <shawnptl8@gmail.com>
2026-02-17 17:29:40 -0800

    remove https

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import fs from "fs"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,9 +7,5 @@ export default defineConfig({
   server: {
     allowedHosts: ["sp-mbp.local"],
     host: true,
-    https: {
-      key: fs.readFileSync("./.cert/key.pem"),
-      cert: fs.readFileSync("./.cert/cert.pem"),
-    },
   },
 })
